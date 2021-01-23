673669b7adfbdc7d0662cfc7aa2e6d99412efb3b
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2021-01-23 19:52:52 -0300

    Improve performance of Max Counters algorithm

--- MaxCounters/index.js
+++ MaxCounters/index.js
@@ -1,17 +1,29 @@
 function solution(N, A) {
     let counters = Array(N).fill(0)
+    let currentMaxValue = 0;
+    let lastMaxValue = 0;
 
     A.forEach(index => {
         const indexToUpdate = index - 1;
 
-        if(index > N) {
-            const maxValue = Math.max(...counters);
-            counters = counters.map(counter => maxValue)
-            return;
-        }
+        if (index > N) {
+            lastMaxValue = currentMaxValue;
+        } else {
+            if(counters[indexToUpdate] < lastMaxValue) {
+                counters[indexToUpdate] = lastMaxValue;
+            }
 
-        counters[indexToUpdate] = counters[indexToUpdate] + 1;   
+            counters[indexToUpdate] = counters[indexToUpdate] + 1;  
+
+            if(counters[indexToUpdate] > currentMaxValue) {
+                currentMaxValue = counters[indexToUpdate];
+            }
+        }
     });
 
-    return counters;
+    return counters.map(counter => {
+        return counter < lastMaxValue
+           ? lastMaxValue
+           : counter;
+    });
 }
